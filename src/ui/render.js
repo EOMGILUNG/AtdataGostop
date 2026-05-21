39cc11faef467290e8237a885bb0aa8501e198df
EOMGILUNG <hyunseohui@gmail.com>
2026-05-21 22:12:51 +0900

    v73: force field 232px height + absolute deck for iOS Safari

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,4 +1,4 @@
-import { CARD_BACK } from "../data/cards.js?v=20260513-73";
+import { CARD_BACK } from "../data/cards.js?v=20260513-74";
 import {
   calculateScore,
   chooseGo,
@@ -10,7 +10,7 @@ import {
   playCard,
   runCpuTurn,
   skipShake,
-} from "../engine/game.js?v=20260513-73";
+} from "../engine/game.js?v=20260513-74";
 
 let matchPrompt = null;
 let bombPrompt = null;
