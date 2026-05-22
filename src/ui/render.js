f9cf3f7976a736798b87edf0a6d18bcee2ec80cf
EOMGILUNG <hyunseohui@gmail.com>
2026-05-22 11:09:39 +0900

    .

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,4 +1,4 @@
-import { CARD_BACK } from "../data/cards.js?v=20260513-82";
+import { CARD_BACK } from "../data/cards.js?v=20260513-84";
 import {
   calculateScore,
   chooseGo,
@@ -10,7 +10,7 @@ import {
   playCard,
   runCpuTurn,
   skipShake,
-} from "../engine/game.js?v=20260513-82";
+} from "../engine/game.js?v=20260513-84";
 
 let matchPrompt = null;
 let bombPrompt = null;
